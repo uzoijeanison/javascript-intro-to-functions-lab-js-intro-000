71693533103736ce5c9f45db832d3fb4678425e9
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-25 14:15:55 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,5 +1,7 @@
 
-describe('shout(string)', function() {
+describe('shout(string)', function shout(string) {
+  return string.toUpperCase()
+}
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO')
   })
