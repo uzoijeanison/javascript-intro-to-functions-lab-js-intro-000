977ccfc74d3854cdb3cb76623e43880fe403ab3e
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-25 14:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string) {
+  if string.lowercase() {
+    return ("I can't hear you!")
+  }
+
+}
