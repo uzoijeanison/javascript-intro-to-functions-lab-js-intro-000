8e803b26999564a098cf1f2ad40b3d3a5707cc13
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-25 14:59:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+function sayHiToGrandma(string) {
+  var uppercase = 'Hello!'
+    uppercase.toUpperCase() === uppercase
+      return "YES INDEED!"
+  var lowercase = 'hello!'
+    lowercase.toLowerCase() === lowercase
+      return "I can\'t hear you!"
+  var mixedCase = 'Hi there!'
+    mixedCase.toLowerCase() === mixedCase // false
+    mixedCase.toUpperCase() === mixedCase // false
+      return "I love you, too."
+
+}
