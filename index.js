42c9a5b61e871319905e6a4f36d93477ebf7ca3c
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-25 15:38:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ function sayHiToGrandma(string) {
   return 'YES INDEED!'
   var lowercase = 'hello!'
   lowercase.toLowerCase() === lowercase
-  return 'I can\'t hear you!' 
+  return 'I can\'t hear you!'
 }
