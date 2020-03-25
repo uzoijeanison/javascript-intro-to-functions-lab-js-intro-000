3f0252f3fd60bf5e5442f27e549a68befbd772c6
Uzoije Anison <uzoijeanison@yahoo.com>
2020-03-25 14:53:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var lowercase = 'hello!', uppercase = 'Hello!', mixedCase = 'Hi there!'
+  var lowercase = 'hello!'
     lowercase.toLowerCase() === lowercase
       return "I can\'t hear you!"
+  var uppercase = 'Hello!'    
     uppercase.toUpperCase() === uppercase
       return "YES INDEED!"
+   var mixedCase = 'Hi there!'    
     mixedCase.toLowerCase() === mixedCase // false
     mixedCase.toUpperCase() === mixedCase // false
       return "I love you, too."
